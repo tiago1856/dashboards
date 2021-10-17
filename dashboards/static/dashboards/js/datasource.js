94d406f5d1e4266671f27fd3feb5b7d800b98ef9
tiago1856 <tiago.ao.santos@gmail.com>
2021-10-17 17:46:26 +0100

    A003

---
+++ dashboards/static/dashboards/js/datasource.js
@@ -0,0 +1,30 @@
+/**
+ * DATA SOURCE MODAL
+ */
+
+
+const QUERY_AREA = $('#data-source-query');
+const TABLE_AREA = $('#data-source-table');
+const NUMBER_LINES = $('#data-source-lines');
+
+// ----------------
+// BUTTONS
+// ----------------
+
+// SAVE QUERY
+$('#data-source-save-btn').on('click',function() {
+})
+
+// NEW QUERY
+$('#data-source-new-btn').on('click',function() {
+})
+
+// EDIT QUERY
+$('#data-source-edit-btn').on('click',function() {
+    QUERY_AREA.removeAttr('disabled');
+})
+
+// EXEC QUERY
+$('#data-source-execute-btn').on('click',function() {
+})
+
